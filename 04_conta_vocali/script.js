77a8f97b7852f7d826250cda86fec8c08fdea9f4
omarseddati16 <omarseddati16@gmail.com>
2025-07-08 16:17:35 +0200

    fourth request

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,20 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vowelsofword(str) {
+  let vowels = [];
+  const vowelList = ["a", "e", "i", "o", "u"];
+  for (let char of str.toLowerCase()) {
+    if (vowelList.includes(char)) {
+      vowels.push(char);
+    }
+  }
+  return vowels;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vowelsofword(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
