0965e6b8d10d4c729e22ee9268c413ba1cd982a3
omarseddati16 <omarseddati16@gmail.com>
2025-07-08 13:53:46 +0200

    second request

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-
+function cutnames(array) {
+  const initials = []
+  for (let i = 0; i < names.length; i++) {
+    const firstletter = array[i].charAt(0).toUpperCase();
+    initials.push(firstletter)
+  }
+  return initials;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(cutnames(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
